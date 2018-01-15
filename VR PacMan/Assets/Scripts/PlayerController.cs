50187f439f73b5c3c652f70f9b321410eb450e9a
Tobias Bauer <tobias.bauer@mytfs.ca>
2018-01-15 10:40:30 -0500

    ADDED - Added the player movement mechanic.

---
+++ VR PacMan/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _rb = null;
+
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private Transform _mainCam = null;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!_mainCam)
+        {
+            _mainCam = Camera.main.transform;
+        }
+
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _rb.velocity = _mainCam.forward * _speed;
+    }
+}
