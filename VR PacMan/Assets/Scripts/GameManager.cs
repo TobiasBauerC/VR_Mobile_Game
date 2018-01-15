72b36e5c4bb5064093e8793a5f29b17e2cf691ed
Tobias Bauer <tobias.bauer@mytfs.ca>
2018-01-15 10:53:33 -0500

    ADDED - GameManager as singleton and included a score variable.

---
+++ VR PacMan/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public int score
+    {
+        private set;
+        get;
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
