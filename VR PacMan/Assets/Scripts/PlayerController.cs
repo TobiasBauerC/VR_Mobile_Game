3785f37b6c3216e56e2892fcadd711dae5cfbecf
Tobias Bauer <TobiasBauer115@gmail.com>
2018-02-19 17:04:47 -0500

    FIX - Portals working as intended

--- VR PacMan/Assets/Scripts/PlayerController.cs
+++ VR PacMan/Assets/Scripts/PlayerController.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody _rb = null;
-	private WaitForSeconds _chaseModeTime = null;
-	private RailInfo _currentHorRail = null;
-	private RailInfo _currentVerRail = null;
-	private Vector3 _upVector = new Vector3(0, 0, 1);
-	private Vector3 _downVector = new Vector3(0, 0, -1);
-	private int _blueGhostPoints = 200;
-	private float _speed = 0.0f;
-	private float _tapElapsedTime = 0.0f;
-	private float _dashElapsedTime = 0.0f;
-	private bool _moving = false;
-	private bool _dashing = false;
-	private bool _tapped = false;
+    private WaitForSeconds _chaseModeTime = null;
+    [SerializeField] private RailInfo _currentHorRail = null;
+    [SerializeField] private RailInfo _currentVerRail = null;
+    private Vector3 _upVector = new Vector3(0, 0, 1);
+    private Vector3 _downVector = new Vector3(0, 0, -1);
+    private int _blueGhostPoints = 200;
+    private float _speed = 0.0f;
+    private float _tapElapsedTime = 0.0f;
+    private float _dashElapsedTime = 0.0f;
+    private bool _moving = false;
+    private bool _dashing = false;
+    private bool _tapped = false;
 
-	[SerializeField] private float _normalSpeed = 5.0f;
-	[SerializeField] private float _dashSpeed = 50.0f;
-	[SerializeField] private float _tapTime = 0.1f;
-	[SerializeField] private float _dashTime = 0.75f;
-	[SerializeField] private Transform _cam = null;
+    [SerializeField] private float _normalSpeed = 5.0f;
+    [SerializeField] private float _dashSpeed = 50.0f;
+    [SerializeField] private float _tapTime = 0.1f;
+    [SerializeField] private float _dashTime = 0.75f;
+    [SerializeField] private Transform _cam = null;
 
     public bool chaseMode { private set; get; }
-	public bool canDash { private set; get; }
+    public bool canDash { private set; get; }
 
 
     // Use this for initialization
@@ -37,7 +37,7 @@ public class PlayerController : MonoBehaviour
 
         _chaseModeTime = new WaitForSeconds(10.0f);
         chaseMode = false;
-		canDash = true;
+        canDash = true;
     }
 
     // Update is called once per frame
@@ -45,66 +45,66 @@ public class PlayerController : MonoBehaviour
     {
         if (GameManager.instance.paused)
             return;
-		
+
         PlayerMovement();
     }
 
     void PlayerMovement()
     {
-		if(_tapped)
-		{
-			_tapElapsedTime += Time.deltaTime;
-		}
+        if (_tapped)
+        {
+            _tapElapsedTime += Time.deltaTime;
+        }
 
-		#if UNITY_ANDROID || UNITY_IOS
+#if UNITY_ANDROID || UNITY_IOS
         for (int i = 0; i < Input.touchCount; ++i)
         {
             if (Input.GetTouch(i).phase == TouchPhase.Began)
-			{
+            {
                 _moving = true;
-				_tapped = true;
-			}
+                _tapped = true;
+            }
             else if (Input.GetTouch(i).phase == TouchPhase.Ended)
-			{
+            {
                 _moving = false;
-				_tapped = false;
+                _tapped = false;
 
-				if(_tapElapsedTime <= _tapTime)
-				{
-					Dash();
-				}
+                if (_tapElapsedTime <= _tapTime)
+                {
+                    Dash();
+                }
 
-				_tapElapsedTime = 0.0f;
-			}
+                _tapElapsedTime = 0.0f;
+            }
         }
-			
-		#endif
-		#if UNITY_EDITOR
+
+#endif
+#if UNITY_EDITOR
         if (Input.GetMouseButtonDown(0))
-		{
+        {
             _moving = true;
-			_tapped = true;
-		}
-		else if(Input.GetMouseButtonUp(0))
-		{
+            _tapped = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
             _moving = false;
-			_tapped = false;
+            _tapped = false;
 
-			if(_tapElapsedTime <= _tapTime)
-			{
-				Dash();
-			}
+            if (_tapElapsedTime <= _tapTime)
+            {
+                Dash();
+            }
 
-			_tapElapsedTime = 0.0f;
-		}
-		#endif
+            _tapElapsedTime = 0.0f;
+        }
+#endif
 
-		_speed = _normalSpeed;
+        _speed = _normalSpeed;
 
-		if(_dashing)
-		{
-			Dash();
-		}
+        if (_dashing)
+        {
+            Dash();
+        }
 
         if (_moving)
         {
@@ -132,24 +132,24 @@ public class PlayerController : MonoBehaviour
             }
             else if (_currentHorRail != null)
             {
-				if (transform.position.z != _currentHorRail.zClamp)
+                if (transform.position.z != _currentHorRail.zClamp)
                 {
-					Vector3 pos = transform.position;
+                    Vector3 pos = transform.position;
 
-					if(transform.position.z - _currentHorRail.zClamp > 0.08f)
-					{
-						pos.z -= 0.05f;
-					}
-					else if(transform.position.z - _currentHorRail.zClamp < -0.08f)
-					{
-						pos.z += 0.05f;
-					}
-					else
-					{
-	                    pos.z = _currentHorRail.zClamp;
-					}
+                    if (transform.position.z - _currentHorRail.zClamp > 0.08f)
+                    {
+                        pos.z -= 0.05f;
+                    }
+                    else if (transform.position.z - _currentHorRail.zClamp < -0.08f)
+                    {
+                        pos.z += 0.05f;
+                    }
+                    else
+                    {
+                        pos.z = _currentHorRail.zClamp;
+                    }
 
-					transform.position = pos;
+                    transform.position = pos;
                 }
 
                 if (lookDir.y == 270)
@@ -167,20 +167,20 @@ public class PlayerController : MonoBehaviour
                 {
                     Vector3 pos = transform.position;
 
-					if(transform.position.x - _currentVerRail.xClamp > 0.08f)
-					{
-						pos.x -= 0.05f;
-					}
-					else if(transform.position.x - _currentVerRail.xClamp < -0.08f)
-					{
-						pos.x += 0.05f;
-					}
-					else
-					{
-                    	pos.x = _currentVerRail.xClamp;
-					}
+                    if (transform.position.x - _currentVerRail.xClamp > 0.08f)
+                    {
+                        pos.x -= 0.05f;
+                    }
+                    else if (transform.position.x - _currentVerRail.xClamp < -0.08f)
+                    {
+                        pos.x += 0.05f;
+                    }
+                    else
+                    {
+                        pos.x = _currentVerRail.xClamp;
+                    }
 
-					transform.position = pos;
+                    transform.position = pos;
                 }
 
                 if (lookDir.y == 0 || lookDir.y == 360)
@@ -195,28 +195,28 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-	private void Dash()
-	{
-		if(!canDash)
-			return;
+    private void Dash()
+    {
+        if (!canDash)
+            return;
 
-		if(_dashTime >= _dashElapsedTime)
-		{
-			GameManager.instance.JustDashed();
-			_dashing = true;
-			_speed = _dashSpeed;
-			_moving = true;
-			_dashElapsedTime += Time.deltaTime;
-		}
-		else
-		{
-			canDash = false;
-			_moving = false;
-			_dashing = false;
-			_dashElapsedTime = 0.0f;
-			StartCoroutine(CanDash());
-		}
-	}
+        if (_dashTime >= _dashElapsedTime)
+        {
+            GameManager.instance.JustDashed();
+            _dashing = true;
+            _speed = _dashSpeed;
+            _moving = true;
+            _dashElapsedTime += Time.deltaTime;
+        }
+        else
+        {
+            canDash = false;
+            _moving = false;
+            _dashing = false;
+            _dashElapsedTime = 0.0f;
+            StartCoroutine(CanDash());
+        }
+    }
 
     void OnTriggerEnter(Collider c)
     {
@@ -241,14 +241,17 @@ public class PlayerController : MonoBehaviour
             _currentVerRail = c.gameObject.GetComponent<RailInfo>();
         }
 
-		if(c.gameObject.tag == "Portal")
-		{
-			PortalTeleport portal = c.gameObject.GetComponent<PortalTeleport>();
-			Vector3 newPos = transform.position;
-			newPos.x = portal.teleDestination.x;
-			newPos.z = portal.teleDestination.z;
-			transform.position = newPos;
-		}
+        if (c.gameObject.tag == "Portal")
+        {
+            Debug.Log("Hit");
+            PortalTeleport portal = c.gameObject.GetComponent<PortalTeleport>();
+            GetComponent<Collider>().enabled = false;
+            StartCoroutine("TimeToTele");
+            Vector3 newPos = transform.position;
+            newPos.x = portal.teleDestination.x;
+            newPos.z = portal.teleDestination.z;
+            transform.position = newPos;
+        }
     }
 
     void OnTriggerExit(Collider c)
@@ -288,10 +291,16 @@ public class PlayerController : MonoBehaviour
         _blueGhostPoints = 200;
     }
 
-	private IEnumerator CanDash()
-	{
-		GameManager.instance.DashRecover();
-		yield return _chaseModeTime;
-		canDash = true;
-	}
+    private IEnumerator CanDash()
+    {
+        GameManager.instance.DashRecover();
+        yield return _chaseModeTime;
+        canDash = true;
+    }
+
+    private IEnumerator TimeToTele()
+    {
+        yield return new WaitForSeconds(1.0f);
+        GetComponent<Collider>().enabled = true;
+    }
 }
